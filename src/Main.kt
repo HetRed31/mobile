ccb19ec6accd36e5b337e33d9816df3542800039
HetRed31 <sanapirat@gmail.com>
2025-10-05 13:57:18 +0300

    Lab2

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+//fun main(args: Array<String>) {
+//    println("Hello, world!")
+//}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i23 = $i")
-    }
-}
+//fun main(args: Array<String>) {
+//    println("Hello, ${args[0]}")
+//}
+
+//fun main(args: Array<String>) {
+//    // Will assign kotlin.Unit
+//    val isUnit = println("This is an expression")
+//    println(isUnit)
+//}
+
+//fun main(args: Array<String>) {
+//    val temperature = 10
+//    val isHot = if (temperature > 50) true else false
+//    println(isHot)
+//}
+
+//fun main(args: Array<String>) {
+//    val temperature = 10
+//    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+//    println(message)
+//}
+
+
+//fun main(args: Array<String>) {
+//    val temperature = 10
+//    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+//    println(message)
+//}
+
+//fun swim(speed: String = "fast") {
+//    println("The fish swims $speed")
+//}
+//
+//fun main() {
+//    swim()   // uses default speed
+//    swim("slow")   // positional argument
+//    swim(speed="turtle-like")   // named parameter
+//}
+
+
