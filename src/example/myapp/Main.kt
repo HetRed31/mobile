0aa633a0d621d81a7d42cd4e05283f5193de68f1
HetRed31 <sanapirat@gmail.com>
2025-10-15 11:23:38 +0300

    Попытка 2

--- src/example/myapp/Main.kt
+++ src/example/myapp/Main.kt
@@ -1,14 +1,20 @@
 package example.myapp
-import example.myapp.decor.Direction
+import example.myapp.decor.Choice
+//import example.myapp.decor.Direction
 
 
 fun main () {
 //    makeFish()
 //     makeDecorations()
-    println(Direction.EAST.name)
-    println(Direction.EAST.ordinal)
-    println(Direction.EAST.degrees)
+//    println(Direction.EAST.name)
+//    println(Direction.EAST.ordinal)
+//    println(Direction.EAST.degrees)
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
 }
+
+
 
 
 //fun buildAquarium() {
--- src/example/myapp/decor/Decoration.kt
+++ src/example/myapp/decor/Decoration.kt
@@ -1,20 +1,33 @@
 package example.myapp.decor
 
-interface FishColor {
-    val color: String
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name:String) = println("My favorite $name")
+    }
 }
 
-// Singleton object
-object GoldColor : FishColor {
-    override val color = "gold"
-}
 
-// Enum class
-enum class Color(val rgb: Int) {
-    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
-}
 
-// Добавьте этот enum Direction
-enum class Direction(val degrees: Int) {
-    NORTH(0), SOUTH(180), EAST(90), WEST(270)
-}
+
+
+
+
+//interface FishColor {
+//    val color: String
+//}
+//
+//// Singleton object
+//object GoldColor : FishColor {
+//    override val color = "gold"
+//}
+//
+//// Enum class
+//enum class Color(val rgb: Int) {
+//    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+//}
+//
+//// Добавьте этот enum Direction
+//enum class Direction(val degrees: Int) {
+//    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+//}
